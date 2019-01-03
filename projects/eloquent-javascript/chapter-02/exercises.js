e807833406d8dd759c0499a2ec36585c49d7cd06
Patrick Ryan <patrickry07@gmail.com>
2019-01-03 19:03:18 +0000

    eloquent javascript submit 1 - 2 done, 1 almost done, 4 started

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -3,23 +3,68 @@
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function triangles() {
-
+function triangles(a){
+  var number = '#'
+  for (var i = 0; i <= a - 1; i++){
+    if( i === 0){
+      console.log('#')
+    }else{
+      console.log(number +='#')
+    }
+  }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function fizzBuzz() {
-  
+function fizzBuzz(a,b) {
+var count = a
+  while (count<b+1){
+      if (count % 3 === 0 && count % 5 === 0){
+          console.log('fizzbuzz');
+      }else if (count % 5 === 0){
+          console.log('buzz');
+      }else if (count % 3 === 0){
+          console.log('fizz');
+      }else{
+          console.log(count)
+      }
+    (count++);
+  }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
-
+function drawChessboard(a) {
+  // creating a for loop to determine the line of console log
+var arrT = [];
+  for (var i = 0; i<=a-1; i++){
+    
+    var arrFinal = [];
+    var arrj = [];
+    var arrk=[];
+      if (i % 2 === 0){
+        for (var j = 0; j < a; j++){
+          if(j % 2 === 0){
+            arrj.push(' ');
+          }else{
+            arrj.push('#');
+          }
+        }arrT.push(arrj.join(''));
+      }else{
+          for (var k = 0; k < a; k++){
+          if(k % 2 === 0){
+            arrk.push('#');
+          }else{
+            arrk.push(' ');
+          }
+        }arrT.push(arrk.join(''));
+      }
+    
+  } console.log(arrT.join('\n')); return arrT.join('\n'); 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
